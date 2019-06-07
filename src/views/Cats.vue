9f52e17780809a60107886d3b031d14bd326ca60
Britt Carter <brittbmh@gmail.com>
2019-06-07 14:19:01 -0500

    map dogs and cats through state

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -4,21 +4,23 @@
     <b-table striped hover :items="cats">
       <template slot="name" slot-scope="data">
         <!-- `data.value` is the value after formatted by the Formatter -->
-        <router-link :to="`/pets/${data.index}`">
-            {{ data.value }}
-        </router-link>
+        <router-link :to="`/pets/cats/${data.index}`">{{ data.value }}</router-link>
       </template>
     </b-table>
   </div>
 </template>
 
 <script>
-import cats from "@/data/cats";
+import { mapState } from "vuex";
+
 export default {
   data() {
-    return {
-      cats
-    };
+    return {};
+  },
+  computed: {
+    ...mapState([
+        "cats"
+        ])
   }
 };
 </script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,19 +1,29 @@
 <template>
     <div>
         <h1>Dogs for Adoption</h1>
-        <b-table striped hover :items="dogs"></b-table>
+        <b-table striped hover :items="dogs">
+            <template slot="name" slot-scope="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        <router-link :to="`/pets/dogs/${data.index}`">{{ data.value }}</router-link>
+      </template>
+        </b-table>
   </div>
     </div>
 </template>
 
 <script>
-import dogs from '@/data/dogs'
+import { mapState } from "vuex";
+
 export default {
     data() {
         return {
-            dogs
         }
-    }
+    },
+    computed: {
+    ...mapState([
+        "dogs"
+        ])
+  }
 }
 </script>
 
